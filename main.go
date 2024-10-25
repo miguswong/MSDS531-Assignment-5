8d7175e6b95a2d02589cd8bd3695f0c7f3c09698
Migus Wong <migus.wong@gmail.com>
2024-10-25 00:11:19 -0500

    Concurrency!

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"sync"
 
 	"github.com/gocolly/colly"
 )
@@ -34,8 +35,66 @@ func removeStopwords(words []string) []string {
 	return result
 }
 
-func main() {
+// takes a url, and creates a struct as the returned value.
+func Scrape(URL string, wg *sync.WaitGroup, sites *[]Site) {
+	defer wg.Done()
+	var targetSite Site
+	//Create Empty struct targetSite
+	targetSite.Url = URL
 
+	//Initialize Scraper
+	c := colly.NewCollector(
+		// Visit only domains: en.wikipedia.org
+		colly.AllowedDomains("en.wikipedia.org"),
+	)
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("visiting", r.URL.String())
+	})
+
+	//Find the tile of the Wiki Page
+	c.OnHTML(".mw-page-title-main", func(e *colly.HTMLElement) {
+		targetSite.Title = e.Text
+	})
+	// Grab Body Text
+	c.OnHTML(".mw-body-content", func(e *colly.HTMLElement) {
+		targetSite.BodyText = e.Text
+	})
+
+	//Parse URL
+	parsedURL, err := url.Parse(URL)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+	}
+	// Split the URL path into segments
+	pathSegments := strings.Split(parsedURL.Path, "/")
+
+	// Initial tags list from URL segments
+	tagsList := []string{pathSegments[1], pathSegments[2]}
+
+	var moreTags []string
+	// Generate more tags
+
+	urlPart := pathSegments[2]
+	words := strings.Split(urlPart, "_")
+	moreTags = removeStopwords(words)
+
+	// Clean and add more tags
+	re := regexp.MustCompile(`[^a-zA-Z]`)
+	for _, tag := range moreTags {
+		cleanedTag := re.ReplaceAllString(tag, "")
+		tagsList = append(tagsList, strings.ToLower(cleanedTag))
+	}
+	targetSite.Tags = tagsList
+
+	c.Visit(URL)
+
+	*sites = append(*sites, targetSite)
+}
+
+func main() {
+	var wg sync.WaitGroup
 	//Initialize item.json file
 	fName := "item.jl"
 	file, err := os.Create(fName)
@@ -64,62 +123,12 @@ func main() {
 
 	//Iterate through URL list and collect data
 	for _, URL := range urls {
-
-		//Create Empty struct targetSite
-		var targetSite Site
-		targetSite.Url = URL
-
-		//Initialize Scraper
-		c := colly.NewCollector(
-			// Visit only domains: en.wikipedia.org
-			colly.AllowedDomains("en.wikipedia.org"),
-		)
-
-		// Before making a request print "Visiting ..."
-		c.OnRequest(func(r *colly.Request) {
-			fmt.Println("visiting", r.URL.String())
-		})
-
-		//Find the tile of the Wiki Page
-		c.OnHTML(".mw-page-title-main", func(e *colly.HTMLElement) {
-			targetSite.Title = e.Text
-		})
-		// Grab Body Text
-		c.OnHTML(".mw-body-content", func(e *colly.HTMLElement) {
-			targetSite.BodyText = e.Text
-		})
-
-		//Parse URL
-		parsedURL, err := url.Parse(URL)
-		if err != nil {
-			fmt.Println("Error parsing URL:", err)
-			return
-		}
-		// Split the URL path into segments
-		pathSegments := strings.Split(parsedURL.Path, "/")
-
-		// Initial tags list from URL segments
-		tagsList := []string{pathSegments[1], pathSegments[2]}
-
-		var moreTags []string
-		// Generate more tags
-
-		urlPart := pathSegments[2]
-		words := strings.Split(urlPart, "_")
-		moreTags = removeStopwords(words)
-
-		// Clean and add more tags
-		re := regexp.MustCompile(`[^a-zA-Z]`)
-		for _, tag := range moreTags {
-			cleanedTag := re.ReplaceAllString(tag, "")
-			tagsList = append(tagsList, strings.ToLower(cleanedTag))
-		}
-		targetSite.Tags = tagsList
-
-		c.Visit(URL)
-		sites = append(sites, targetSite)
+		wg.Add(1)
+		go Scrape(URL, &wg, &sites)
 	}
 
+	//
+	wg.Wait()
 	enc := json.NewEncoder(file)
 	enc.SetIndent("", "  ")
 
